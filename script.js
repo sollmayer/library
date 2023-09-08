5e2d638e05fcc68cd33b37ff0f1da7a7085a48c6
Serhii <stupanenko.serhii@gmail.com>
2023-09-08 18:04:17 +0300

    update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const myLibrary = [];
+let myLibrary = [];
+
+const books_div = document.querySelector('.books');
+const showForm = document.querySelector('#showForm')
+const formDialog = document.querySelector("#form_dialog");
+const outputBox = document.querySelector("output");
+const addBookBtn = formDialog.querySelector("#addBook_btn");
+
+showForm.addEventListener('click', ()=>formDialog.showModal())
 
 function Book(title, author, pages, status) {
     this.title = title;
@@ -14,32 +22,49 @@ function addBookToLibrary() {
     let bookStatus = document.querySelector("#status").value;
 
     let book = new Book(bookTitle, bookAuthor, bookPages, bookStatus);
-    myLibrary.push(book);
+    
+    if(!myLibrary.some(item => item.title === book.title)){
+        myLibrary.push(book);
+    }else return;
 }
-const books_div = document.querySelector('.books');
+
+
+function clearLibrary(){
+    // while (books_div.firstChild) {
+    //     books_div.removeChild(books_div.lastChild);
+    // }
+    document.querySelector('.books').innerHTML = '';
+
+}
 
 function showBooks(){
-
+    clearLibrary();
     for(let book of myLibrary){
+
         console.log(book);
         let div = document.createElement('div');
         let title = document.createElement('p');
         let author = document.createElement('p');
         let pages = document.createElement('p');
         let status = document.createElement('p');
+
         title.textContent = book.title;
         author.textContent = book.author;
         pages.textContent = book.pages;
         status.textContent = book.status;
+
         div.appendChild(title);
         div.appendChild(author);
         div.appendChild(pages);
         div.appendChild(status);
+        div.appendChild(delete_btn);
         books_div.appendChild(div);
     }
 
 }
-const addButton = document.querySelector('#addBook');
-addButton.addEventListener('click', ()=>{
+addBookBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
     addBookToLibrary();
+    formDialog.close();
+    showBooks();
 })
