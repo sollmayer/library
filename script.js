b8369daaba04ea103518fd2f2490cb640e5c4bc3
Serhii <stupanenko.serhii@gmail.com>
2023-09-10 12:59:28 +0300

    update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const formDialog = document.querySelector("#form_dialog");
 const outputBox = document.querySelector("output");
 const addBookBtn = formDialog.querySelector("#addBook_btn");
 
+let del_btn;
 showForm.addEventListener('click', ()=>formDialog.showModal())
 
 function Book(title, author, pages, status) {
@@ -47,11 +48,14 @@ function showBooks(){
         let author = document.createElement('p');
         let pages = document.createElement('p');
         let status = document.createElement('p');
+        let delete_btn = document.createElement('button'); 
 
         title.textContent = book.title;
         author.textContent = book.author;
         pages.textContent = book.pages;
         status.textContent = book.status;
+        delete_btn.textContent = 'delete';
+        delete_btn.setAttribute(`data-title`,book.title);
 
         div.appendChild(title);
         div.appendChild(author);
@@ -60,8 +64,21 @@ function showBooks(){
         div.appendChild(delete_btn);
         books_div.appendChild(div);
     }
+    addButtons();
 
 }
+
+function addButtons(){
+    document.querySelectorAll('.books div button').forEach(btn => {
+        btn.addEventListener('click',()=>{
+            console.log("delete_btn clicked", btn.dataset.title)
+            myLibrary = myLibrary.filter(book => book.title !== btn.dataset.title);
+            showBooks();
+        })
+    })
+
+}
+
 addBookBtn.addEventListener('click', (e)=>{
     e.preventDefault();
     addBookToLibrary();
